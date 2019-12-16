a2000cdca8d590ce1426a8c42ecc168e4042e6d8
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-16 18:09:42 +0530

    palindrome problem solving

---
+++ palindrome/index.js
@@ -0,0 +1,22 @@
+// Directions 
+// Given a string, return true if the string is a palindrome 
+// or false if it is not.  Palindromes are strings that
+// Examples
+// palindrome('abba') === true
+// palindrome('abcdfh') ===  
+
+// solution 1
+function palindrome(str) {
+    const rev = str.split('').reverse().join('');
+    return str === rev;
+}
+
+// solution 2
+function palindrome(str) {
+   return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
+
+
+console.log(palindrome('abba'));
