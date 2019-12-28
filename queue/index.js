1172137e9054b89fbfb10de044e7e26548c267de
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-28 16:44:43 +0530

    queue data structure

---
+++ queue/index.js
@@ -0,0 +1,23 @@
+// --------- Description---------------
+// create a queue data structure. The queue
+// should be class with methods add and remove
+// adding to the queue should store an element until
+// it is removed
+// ----------- Examples ----------
+// const q = new Queue();
+// q.add(1);
+// q.remove(); //remove 1;
+
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+
+    add (val) {
+        this.data.unshift(val);
+    }
+
+    remove() {
+        this.data.pop();
+    }
+}
